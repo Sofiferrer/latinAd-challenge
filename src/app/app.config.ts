f0eddf71c1fdbed1731b239c6acca68923e9a0ce
sofia <sofia@blockinar.com>
2024-05-02 20:10:54 -0300

    requests interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,10 +8,25 @@ import { registerLocaleData } from '@angular/common';
 import es from '@angular/common/locales/es';
 import { FormsModule } from '@angular/forms';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideStore, provideState } from '@ngrx/store';
+import { screenReducer } from './store/screen/screen.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { ScreenEffect } from './store/screen/screen.effect';
+import { customInterceptor } from './service/custom.interceptor';
 
 registerLocaleData(es);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideNzI18n(es_ES), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideNzI18n(es_ES),
+    importProvidersFrom(FormsModule),
+    provideAnimationsAsync(),
+    provideHttpClient(withInterceptors([customInterceptor])),
+    provideStore(),
+    provideState({ name: 'screen', reducer: screenReducer }),
+    provideEffects(ScreenEffect),
+  ],
 };
